1d11d0308c8c6225d2cfbca69fb1fb4adbeda8bf
tahagiremle <tahagiremle@gmail.com>
2024-10-22 23:06:05 +0300

    add addUser

--- lib/features/user/data/repositories/user_repository.dart
+++ lib/features/user/data/repositories/user_repository.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_application_1/core/database/database_helper.dart';
+import 'package:flutter_application_1/features/user/data/models/user.dart';
 
 class UserRepository {
   final DatabaseHelper databaseHelper;
 
   UserRepository(this.databaseHelper);
+
+  Future<int> addUser(User user) async {
+    return await databaseHelper.insertUser(user);
+  }
 }
