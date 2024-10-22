17ac5d7e191f5fda2062c5825f598218b3778ce1
tahagiremle <tahagiremle@gmail.com>
2024-10-22 23:04:04 +0300

    create UserRepository

---
+++ lib/features/user/data/repositories/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_application_1/core/database/database_helper.dart';
+
+class UserRepository {
+  final DatabaseHelper databaseHelper;
+
+  UserRepository(this.databaseHelper);
+}
