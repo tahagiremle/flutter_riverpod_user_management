a9891325b8561bee033b688a44aebc1976b00a0f
tahagiremle <tahagiremle@gmail.com>
2024-10-24 12:15:09 +0300

    add user_provider

---
+++ lib/features/presentation/providers/user_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_application_1/core/database/database_helper.dart';
+import 'package:flutter_application_1/features/user/data/models/user.dart';
+import 'package:flutter_application_1/features/user/data/repositories/user_repository.dart';
+import 'package:riverpod/riverpod.dart';
+
+final userProvider = StateNotifierProvider<UserNotifier, List<User>>((ref) {
+  final userRepository = ref.watch(userRepositoryProvider);
+  return UserNotifier(userRepository);
+});
+
+final userRepositoryProvider = Provider<UserRepository>((ref) {
+  final databaseHelper = DatabaseHelper();
+  return UserRepository(databaseHelper);
+});
+
+class UserNotifier extends StateNotifier<List<User>> {
+  final UserRepository userRepository;
+
+  UserNotifier(this.userRepository) : super([]);
+
+  Future<void> loadUsers() async {
+    final users = await userRepository.fetchUser();
+    state = users;
+  }
+
+  Future<void> addUser(User user) async {
+    await userRepository.addUser(user);
+    await loadUsers();
+  }
+
+  Future<void> updateUser(User user) async {
+    await userRepository.updateUser(user);
+    await loadUsers();
+  }
+
+  Future<void> deleteUser(int id) async {
+    await userRepository.removeUser(id);
+    await loadUsers();
+  }
+}
